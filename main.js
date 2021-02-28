1f302d177d3a4cf21e5f74604bcafd329ec51467
Jesse van Papenrecht <J.C.vanPapenrecht@student.tudelft.nl>
2021-02-28 15:47:17 +0100

    Changed businesscard to incoive

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var fs = require("fs");
 var pdf = require("html-pdf");
-var html = fs.readFileSync("./test/businesscard.html", "utf8");
+var html = fs.readFileSync("./test/factuur-template.html", "utf8");
 var options = { format: "Letter" };
  
-pdf.create(html, options).toFile("./businesscard.pdf", function (err, res) {
+// Factuur nummer in naam als parameter
+pdf.create(html, options).toFile("./factuur.pdf", function (err, res) {
   if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
+  console.log(res);
 });
