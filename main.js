5657596bf97d21588fdd832d4311e701d0a6901f
Jay Doerga <jay@jjdeveloping.nl>
2021-03-01 20:37:24 +0100

    Express

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 var fs = require("fs");
 var pdf = require("html-pdf");
-<<<<<<< HEAD
-var html = fs.readFileSync("./test/businesscard.html", "utf8");
-var options = { format: "Legal" };
-=======
 var html = fs.readFileSync("./test/factuur-template.html", "utf8");
-var options = { format: "Letter" };
->>>>>>> d296165e3ba2e9172702be4ff944db2bfae52edd
+var options = { format: "A4" };
  
 // Factuur nummer in naam als parameter
 pdf.create(html, options).toFile("./factuur.pdf", function (err, res) {
