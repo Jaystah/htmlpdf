042e67fe59f3749d6a9ca8217275046a74454dc8
Jesse van Papenrecht <J.C.vanPapenrecht@student.tudelft.nl>
2021-03-06 20:41:33 +0100

    Created a working instance of creating an invoice

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 const cors = require('cors')
+let ejs = require('ejs');
+
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 app.use(cors())
@@ -16,6 +19,42 @@ app.get('/makeinvoice', (req, res) => {
     file.pipe(res);
 })
 
+app.get('/test', (req, res) => {
+    let invoice = req.query;
+    
+    invoice = JSON.parse(invoice.json).invoice;
+    let d = new Date();
+    invoice.today = convertDate(d.toISOString().substr(0, 10));
+    d.setDate(d.getDate() + parseInt(invoice.payment_term))
+    invoice.payment_term = convertDate(d.toISOString().substr(0, 10));
+
+    var html;
+    ejs.renderFile('views/test.ejs', {invoice: invoice}, {}, function(err, str){
+        html = str;
+        console.log("err",err);
+    });
+
+    var fs = require("fs");
+    var pdf = require("html-pdf");
+    var options = { format: "A4" };
+    
+    pdf.create(html, options).toFile("./factuur.pdf", function (err, res) {
+    if (err) return console.log(err);
+        console.log(res);
+    });
+    var file = fs.createReadStream('./factuur.pdf');
+    var stat = fs.statSync('./factuur.pdf');
+    res.setHeader('Content-Length', stat.size);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+    file.pipe(res);
+  });
+
 app.listen(PORT, () => {
     console.log("Listening on port", PORT)
 })
+
+function convertDate(dateString){
+    var p = dateString.split(/\D/g)
+    return [p[2],p[1],p[0] ].join("-")
+}
