774806181610f4ec3bfbfae5261877401fab478f
Jay Doerga <jay@jjdeveloping.nl>
2021-03-08 20:03:38 +0100

    HTML change + loader

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,141 +1,221 @@
 <template>
-<div id="form">
+  <div id="form">
     <title>Factuur maken</title>
-        <form @submit.prevent="sendRequest">
-            <table>
-                <tr>
-                    <td>
-                        <label for="invoice_number">Factuurnummer:</label>
-                    </td>
-                    <td>
-                        <input type="number" placeholder="Factuurnummer" name="invoice_number" v-model="invoice.invoice_number" required>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="customer_name">Naam klant:</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Naam klant" name="customer_name" v-model="invoice.customer_name" required>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="customer_address">Adres klant:</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Straat 123" name="customer_address1" v-model="invoice.customer_address1" required>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>
-                        <input type="text" placeholder="1111AA Alkmaar" name="customer_address2" v-model="invoice.customer_address2" required>
-                    </td>
-                <tr>
-                    <td>
-                        <label for="payment_term">Betaal termijn in dagen:</label>
-                    </td>
-                    <td>
-                        <input type="number" placeholder="Betaaltermijn in dagen" value="7" name="payment_term" v-model="invoice.payment_term" required>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="products">Services:</label>
-                    </td>
-                    <td>
-                        <textarea placeholder="1;website;600\n" name="products" v-model="invoice.products" required></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="note">Opmerkingen:</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Opmerkingen" v-model="invoice.note" name="note">
-                    </td>
-                </tr>
-            </table>
-            <input type="submit" value="Maak factuur aan">
-        </form>
-        </div>
+    <form @submit.prevent="sendRequest">
+      <table>
+        <tr>
+          <td>
+            <label for="invoice_number">Factuurnummer:</label>
+          </td>
+          <td>
+            <input
+              type="number"
+              placeholder="Factuurnummer"
+              name="invoice_number"
+              v-model="invoice.invoice_number"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="customer_name">Naam klant:</label>
+          </td>
+          <td>
+            <input
+              type="text"
+              placeholder="Naam klant"
+              name="customer_name"
+              v-model="invoice.customer_name"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="customer_address">Adres klant:</label>
+          </td>
+          <td>
+            <input
+              type="text"
+              placeholder="Straat 123"
+              name="customer_address1"
+              v-model="invoice.customer_address1"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <input
+              type="text"
+              placeholder="1111AA Alkmaar"
+              name="customer_address2"
+              v-model="invoice.customer_address2"
+              required
+            />
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <label for="payment_term">Betaal termijn in dagen:</label>
+          </td>
+          <td>
+            <input
+              type="number"
+              placeholder="Betaaltermijn in dagen"
+              value="7"
+              name="payment_term"
+              v-model="invoice.payment_term"
+              required
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="products">Services:</label>
+          </td>
+          <td>
+            <textarea
+              placeholder="1;website;600\n"
+              name="products"
+              v-model="invoice.products"
+              required
+            ></textarea>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label for="note">Opmerkingen:</label>
+          </td>
+          <td>
+            <input
+              type="text"
+              placeholder="Opmerkingen"
+              v-model="invoice.note"
+              name="note"
+            />
+          </td>
+        </tr>
+      </table>
+      <input type="submit" value="Maak factuur aan" />
+    </form>
+    <div v-if="isLoading" id="wrapper">
+      <span class="pulser"></span>
+    </div>
+  </div>
 </template>
 
 <script>
-import * as FileSaver from 'file-saver';
-import axios from 'axios';
+import * as FileSaver from "file-saver";
+import axios from "axios";
 export default {
-    data() {
-        return {
-            invoice: {
-                invoice_number: '',
-                customer_name: '',
-                customer_address1: '',
-                customer_address2: '',
-                payment_term: '',
-                products: '',
-                note: ''
-            }
-        }
-    },
-    methods: {
-        sendRequest: function() {
-            let tempArray = this.invoice.products.split('\n');
-            let products = [];
-            for(let productLine of tempArray) {
-                let productArray = productLine.split(';');
-                let product = {
-                    "aantal": productArray[0],
-                    "dienst": productArray[1],
-                    "prijs": productArray[2]
-                }
-                products.push(product);
-            }
-            this.invoice.products = products;
-            let invoice = this.invoice;
-            let json = JSON.stringify(invoice)
+  data() {
+    return {
+      isLoading: false,
+      invoice: {
+        invoice_number: "",
+        customer_name: "",
+        customer_address1: "",
+        customer_address2: "",
+        payment_term: "",
+        products: "",
+        note: "",
+      },
+    };
+  },
+  methods: {
+    sendRequest: function () {
+      this.isLoading = true;
+      let tempArray = this.invoice.products.split("\n");
+      let products = [];
+      for (let productLine of tempArray) {
+        let productArray = productLine.split(";");
+        let product = {
+          aantal: productArray[0],
+          dienst: productArray[1],
+          prijs: productArray[2],
+        };
+        products.push(product);
+      }
+      this.invoice.products = products;
+      let invoice = this.invoice;
+      let json = JSON.stringify(invoice);
 
-            axios.get('http://localhost:3000/test', {
-                params: {
-                    json
-                }
-            }).then(() => {
-                setTimeout(() => {  
-                    fetch('/makeinvoice',{
-                    headers:{
-                        "Content-Type": "application/json"
-                    },
-                    method: "get"
-                }).then(res=>res.blob()).then((res=>{
-                    console.log(res)
-                    FileSaver.saveAs(res, 'kameel');
-                }))
-                 }, 5000);
-                
-            })  
-        }
-    }
-}
+      axios
+        .get("http://localhost:3000/test", {
+          params: {
+            json,
+          },
+        })
+        .then(() => {
+          setTimeout(() => {
+            fetch("/makeinvoice", {
+              headers: {
+                "Content-Type": "application/json",
+              },
+              method: "get",
+            })
+              .then((res) => res.blob())
+              .then((res) => {
+                console.log(res);
+                FileSaver.saveAs(res, "kameel");
+                this.isLoading = false;
+              });
+          }, 5000);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
     * {
-        font-family: Poppins;
+    font-family: Poppins;
     }
     input[type="submit"] {
-        transition-duration: 0.3s;
-        background-color: black;
-        color: white;
-        border: 2px solid gray;
-        border-radius: 5px;
+    transition-duration: 0.3s;
+    background-color: black;
+    color: white;
+    border: 2px solid gray;
+    border-radius: 5px;
     }
     input[type="submit"]:hover {
-        transition-duration: 0.3s;
-        background-color: white;
-        color: black;
-        cursor: pointer;
+    transition-duration: 0.3s;
+    background-color: white;
+    color: black;
+    cursor: pointer;
     }
+    #wrapper {
+    width: 90%;
+    min-width: 600px;
+    min-height: 1px;
+    margin: 50px auto;
+    }
+    .pulser {
+  display: block;
+  float: left;
+  margin: 30px;
+  border-radius: 100px;
+  width: 30px;
+  height: 30px;
+  border: 10px solid #ADE;
+  animation-name: pulse, fadeout;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in;
+}
+@keyframes pulse {
+  0% { transform: scale(0); }
+  100% { transform: scale(1); -webkit-filter: blur(5px); }
+}
+
+@keyframes fadeout {
+  85% { opacity: 1; }
+  100% { opacity: 0; }
+}
 </style>
 
